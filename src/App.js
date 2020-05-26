f61df21a7ad0d12555a5121ceaa151abb4820dd9
ebisLab <ebaiden@techie.com>
2020-05-26 17:50:18 -0400

    grid set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const rows_number = 20; //TO BE DYNAMIC
+  const cols_number = 20; //TO BE DYNAMIC
+
+  const createGrid=()=>{
+    const rows = []
+
+    for(let i=0; i< rows_number; i++){
+      rows.push(Array.from(Array(rows_number), ()=>0))
+    }
+    return rows
+  }
+
+  const [grid, setGrid]= useState(createGrid)
+
+
+  // console.log(grid)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Conway Game of Life</h1>
+      <button>Play</button>
+
+{/* grid */}
+<div 
+style={{display: 'grid',
+justifyContent: 'center',
+padding: 10,
+gridTemplateColumns: `repeat(${cols_number}, 21px)`}}>
+  {grid.map((rows,i)=>(
+    //for every row render a column
+    rows.map((cols,j) =>(
+      <div 
+      key={`${i}-${j}`}
+      style={{width: 20, 
+        height: 20, border: '1px solid black'}}>{cols}</div>
+    ))
+  // <div style={{border: '1px solid black', display: 'grid'}}>{row}</div>
+  ))}
+</div>
     </div>
   );
 }
